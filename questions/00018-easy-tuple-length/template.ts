376a5ee45a463c22fcca8281d9754d0882855907
BayesWang <bayes0621@gmail.com>
2022-05-04 02:37:27 +0800

    fix: 018

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,3 +1,5 @@
-type Length<T extends ReadonlyArray<keyof any>> = T extends { length: number }
-  ? T['length']
-  : never
+// type Length<T extends ReadonlyArray<keyof any>> = T extends { length: number }
+//   ? T['length']
+//   : never
+
+type Length<T extends ReadonlyArray<any>> = T['length']
