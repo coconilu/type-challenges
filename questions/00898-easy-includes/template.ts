5b1b5be57853cc1363587536ec0111156a45b512
BayesWang <bayes0621@gmail.com>
2022-05-05 00:19:42 +0800

    feat: 898

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,11 @@
-type Includes<T extends readonly any[], U> = any
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false
+
+type Includes<T extends any[], U> = T extends [infer R, ...infer L]
+  ? Equal<R, U> extends true
+    ? true
+    : Includes<L, U>
+  : false
