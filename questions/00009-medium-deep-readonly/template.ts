74d2f09eb8bde1aefef54ad19b2f6bbec0bd0b88
BayesWang <bayes0621@gmail.com>
2022-05-08 16:03:40 +0800

    feat: 009

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,9 @@
-type DeepReadonly<T> = any
+// type DeepReadonly<T> = {
+//   [K in keyof T]-?: T[K] extends object ? DeepReadonly<T[K]> : T[K]
+// }
+
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: keyof T[K] extends undefined
+    ? T[K]
+    : DeepReadonly<T[K]>
+}
