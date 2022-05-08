cd597306bbbf9750e714136f3fdca7728d51bc41
BayesWang <bayes0621@gmail.com>
2022-05-08 15:09:31 +0800

    feat: 003

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -2,6 +2,10 @@
 //   [Key in keyof T]: Key extends K ? never : T[Key]
 // }
 
+// type MyOmit<T, K extends keyof T> = {
+//   [Key in keyof T as Key extends K ? never : Key]: T[Key]
+// }
+
 type MyOmit<T, K extends keyof T> = {
-  [Key in keyof T as Key extends K ? never : Key]: T[Key]
+  [Key in Exclude<keyof T, K>]: T[Key]
 }
